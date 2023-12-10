15ef2c35ff2c4c92a6364f910bf0d8e4cf54ce3d
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2023-12-09 23:26:19 -0600

    shorten template parameter lists

--- src/ricci/axioms/arithmetic.hpp
+++ src/ricci/axioms/arithmetic.hpp
@@ -15,61 +15,63 @@
 namespace ricci::axioms::impl_ {
 namespace operators = ricci::core::operators;
 
+using ricci::concepts::Arithmetic;
+
 // associativity
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Associative_<operators::add, T_Arg_1, T_Arg_2> : std::true_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Associative_<operators::multiply, T_Arg_1, T_Arg_2> : std::true_type {};
 
 // closure
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Closed_<operators::add, T_Arg_1, T_Arg_2> : std::true_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Closed_<operators::subtract, T_Arg_1, T_Arg_2> : std::true_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 requires (std::unsigned_integral<T_Arg_1> or std::unsigned_integral<T_Arg_2>)
 struct Is_Closed_<operators::subtract, T_Arg_1, T_Arg_2> : std::false_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Closed_<operators::multiply, T_Arg_1, T_Arg_2> : std::true_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Closed_<operators::divide, T_Arg_1, T_Arg_2> : std::true_type {};
 
 // commutativity
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Commutative_<operators::add, T_Arg_1, T_Arg_2> : std::true_type {};
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Is_Commutative_<operators::multiply, T_Arg_1, T_Arg_2> : std::true_type {};
 
 // identity
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Identity_<operators::add, T_Arg_1, T_Arg_2> {
     using type = std::common_type_t<T_Arg_1, T_Arg_2>;
     static constexpr xt::Constant<type{0}> value{};
 };
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Identity_<operators::subtract, T_Arg_1, T_Arg_2> {
     using type = std::common_type_t<T_Arg_1, T_Arg_2>;
     static constexpr xt::Constant<type{0}> value{};
 };
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Identity_<operators::multiply, T_Arg_1, T_Arg_2> {
     using type = std::common_type_t<T_Arg_1, T_Arg_2>;
     static constexpr xt::Constant<type{1}> value{};
 };
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Identity_<operators::divide, T_Arg_1, T_Arg_2> {
     using type = std::common_type_t<T_Arg_1, T_Arg_2>;
     static constexpr xt::Constant<type{1}> value{};
@@ -77,17 +79,17 @@ struct Identity_<operators::divide, T_Arg_1, T_Arg_2> {
 
 // inverse
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Inverse_Operation_<operators::add, T_Arg_1, T_Arg_2> {
     static constexpr auto value = operators::subtract;
 };
 
-template <concepts::Arithmetic T_Arg_1, concepts::Arithmetic T_Arg_2>
+template <Arithmetic T_Arg_1, Arithmetic T_Arg_2>
 struct Inverse_Operation_<operators::subtract, T_Arg_1, T_Arg_2> {
     static constexpr auto value = operators::add;
 };
 
-template <concepts::Arithmetic T>
+template <Arithmetic T>
 struct Inverse_Element_<operators::add, T> {
     static constexpr T apply (T value) {
         return -value;
